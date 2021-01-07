9f8bd83609bbc4d9025d959b9fee10932e44c1a0
Yannis <admin@coding-hard.com>
2021-01-07 23:08:34 +0100

    need for lru test

--- packageObjects/order.go
+++ packageObjects/order.go
@@ -12,3 +12,25 @@ type Order struct {
 	AlbumIDs []int
 	PhotoIDs []int
 }
+
+func createNewOrder(userID int, albumIDs []int, photoIDs []int) *Order {
+	currentTime := time.Now()
+	timeFormatted := currentTime.Format("2006.01.02 15:04:05")
+
+	randInt := getNewOrderID()
+
+	var order = Order{
+		ID: randInt,
+		UserID: userID,
+		Date: timeFormatted,
+		AlbumIDs: albumIDs,
+		PhotoIDs: photoIDs,
+	}
+
+	return &order
+}
+
+func getNewOrderID() int {
+	randInt := rand.Intn(100)
+	return randInt
+}
