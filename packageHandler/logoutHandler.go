e10dc3ca0ab7f4b781756fbd322a4379d86ea2ab
Johannes Weis <joh.weis.18@lehre.mosbach.dhbw.de>
2021-01-08 12:34:31 +0100

    add logout

---
+++ packageHandler/logoutHandler.go
@@ -0,0 +1,17 @@
+package packageHandler
+
+import (
+	"net/http"
+	"time"
+)
+
+func LogoutHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+
+	if r.Method == "GET" {
+		expiration := time.Now().Add(1)
+		cookie := http.Cookie{Name: "csrftoken", Value: "-", Expires: expiration}
+		http.SetCookie(w, &cookie)
+		http.Redirect(w, r, "/login", http.StatusSeeOther)
+	}
+}
