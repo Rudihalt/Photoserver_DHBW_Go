b310d9c62565acf59b5314d2b115ae2eff8d28b6
Yannis <admin@coding-hard.com>
2021-01-09 18:06:35 +0100

    add comment feature

--- packageHandler/imageHandler.go
+++ packageHandler/imageHandler.go
@@ -1,9 +1,16 @@
 package packageHandler
 
 import (
+	"log"
 	"net/http"
 	"photoserver/packageObjects"
 )
+
+type ImageShowData struct {
+	Hash string
+	Photo packageObjects.Photo
+	Comments []packageObjects.Comment
+}
 
 func ImageHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
@@ -15,7 +22,49 @@ func ImageHandler(w http.ResponseWriter, r *http.Request) {
 		NavData = NavBarData{Username: user.Username}
 		err := NavTemplate.Execute(w, NavData)
 
-		err = ImageTemplate.Execute(w, NavData)
+		q := r.URL.Query()
+		imageGet := q.Get("image")
+
+		if imageGet == "" {
+			http.Redirect(w, r, "/gallery", http.StatusSeeOther)
+		}
+
+		allPhotos := packageObjects.GetAllPhotosByUser(user.Username)
+		photo := packageObjects.GetPhotoByUserAndHash(allPhotos, imageGet)
+
+		if photo == nil {
+			http.Redirect(w, r, "/gallery", http.StatusSeeOther)
+		}
+
+		if r.Method == "POST" {
+			if err := r.ParseForm(); err != nil {
+				log.Fatalln(err)
+				return
+			}
+			comment := r.FormValue("comment")
+			log.Println("comment:", comment)
+
+			if comment != "" && len(comment) > 0 {
+				addComment := packageObjects.AddComment(user.Username, imageGet, comment)
+
+				if addComment == nil {
+					log.Println("Error adding comment")
+				} else {
+					log.Println("Successfully adding comment")
+				}
+			}
+		}
+
+		allComments := packageObjects.GetAllCommentsByUser(user.Username)
+		comments := packageObjects.FilterAllCommentsByHash(allComments, imageGet)
+
+		imageShowData := ImageShowData{
+			Hash: photo.Hash,
+			Photo: *photo,
+			Comments: *comments,
+		}
+
+		err = ImageTemplate.Execute(w, imageShowData)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
