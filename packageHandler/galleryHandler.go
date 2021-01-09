c928aa7b2a81504a20f2b0285c5a05170474acbc
Johannes Weis <joh.weis.18@lehre.mosbach.dhbw.de>
2021-01-09 20:01:06 +0100

    add gallery functionality

--- packageHandler/galleryHandler.go
+++ packageHandler/galleryHandler.go
@@ -3,7 +3,15 @@ package packageHandler
 import (
 	"net/http"
 	"photoserver/packageObjects"
+	"strconv"
 )
+
+type GalleryData struct {
+	Username string
+	Photos   *[]packageObjects.Photo
+	Amount   int
+	Pages    []int
+}
 
 func GalleryHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
@@ -15,7 +23,31 @@ func GalleryHandler(w http.ResponseWriter, r *http.Request) {
 		NavData = NavBarData{Username: user.Username}
 		err := NavTemplate.Execute(w, NavData)
 
-		err = GalleryTemplate.Execute(w, NavData)
+		GData := GalleryData{Username: user.Username,
+			Amount: 0,
+		}
+
+		pages := packageObjects.GetPhotoPageAmount(user.Username)
+		if pages != 0 {
+			page := r.URL.Query().Get("p")
+			p, _ := strconv.Atoi(page)
+			if p == 0 {
+				p = 1
+			}
+			if p > pages {
+				p = pages
+			}
+
+			var paging []int
+			for i := 0; i < pages; i++ {
+				paging = append(paging, i+1)
+			}
+			GData.Photos = packageObjects.GetPhotosForPage(user.Username, p)
+			GData.Amount = len(*GData.Photos)
+			GData.Pages = paging
+		}
+
+		err = GalleryTemplate.Execute(w, GData)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
