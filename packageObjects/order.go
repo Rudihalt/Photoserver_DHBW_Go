e73bb18d27bfbb2258b4e2ca4c96c17c5f79ba01
Yannis <admin@coding-hard.com>
2021-01-09 02:24:58 +0100

    implement order stuff

--- packageObjects/order.go
+++ packageObjects/order.go
@@ -1,36 +1,86 @@
 package packageObjects
 
 import (
-	"math/rand"
-	"time"
+	"encoding/json"
+	"io/ioutil"
+	"os"
 )
 
-type Order struct {
-	ID int
-	UserID int
-	Date string
-	AlbumIDs []int
-	PhotoIDs []int
+type OrderElement struct {
+	Hash string `json:"hash"`
+	Amount int `json:"amount"`
+	Format string `json:"format"`
 }
 
-func createNewOrder(userID int, albumIDs []int, photoIDs []int) *Order {
-	currentTime := time.Now()
-	timeFormatted := currentTime.Format("2006.01.02 15:04:05")
+func GetAllOrderElementsByUser(username string) *[]OrderElement {
+	var orderElements []OrderElement
+	var orderElementsFile []byte
 
-	randInt := getNewOrderID()
+	orderElementsFile, err := ioutil.ReadFile("static/data/order_" + username + ".json")
 
-	var order = Order{
-		ID: randInt,
-		UserID: userID,
-		Date: timeFormatted,
-		AlbumIDs: albumIDs,
-		PhotoIDs: photoIDs,
+	err = json.Unmarshal(orderElementsFile, &orderElements)
+
+	if err != nil {
+		// panic(err)
 	}
 
-	return &order
+	return &orderElements
 }
 
-func getNewOrderID() int {
-	randInt := rand.Intn(100)
-	return randInt
+func saveOrderElements(username string, orderElements *[]OrderElement) {
+	orderElementsJson, err := json.MarshalIndent(orderElements, "", "\t")
+	if err != nil {
+		panic(err)
+	}
+
+	err = ioutil.WriteFile("static/data/order_" + username + ".json", orderElementsJson, 0644)
+	if err != nil {
+		panic(err)
+	}
 }
+
+func AddOrderElement(username string, hash string, amount int, format string) *OrderElement {
+	if format != "3x4" && format != "16x9" && format != "1x2" {
+		return nil
+	}
+
+	currentOrderElements := *GetAllOrderElementsByUser(username)
+
+	var orderElement = OrderElement {
+		Hash: hash,
+		Amount: amount,
+		Format: format,
+	}
+
+	currentOrderElements = append(currentOrderElements, orderElement)
+
+	saveOrderElements(username, &currentOrderElements)
+
+	return &orderElement
+}
+
+func deleteOrderElementByHash(username string, hash string) {
+	var newOrderElements []OrderElement
+	currentOrderElements := *GetAllOrderElementsByUser(username)
+
+	for _, orderElement := range currentOrderElements {
+		if orderElement.Hash != hash {
+			newOrderElements = append(newOrderElements, orderElement)
+		}
+	}
+
+	saveOrderElements(username, &newOrderElements)
+}
+
+// https://stackoverflow.com/questions/37334119/how-to-delete-an-element-from-a-slice-in-golang
+func removeElementByIndex(slice []int, s int) []int {
+	return append(slice[:s], slice[s+1:]...)
+}
+
+func deleteFullOrder(username string) {
+	err := os.Remove("static/data/order_" + username + ".json")
+	if err != nil {
+		panic(err)
+	}
+}
+
