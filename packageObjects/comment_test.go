30a4efbf46e85b6189f20b8bfb40a7375588b672
Yannis <admin@coding-hard.com>
2021-01-10 18:22:04 +0100

    add tests for comment

--- packageObjects/comment_test.go
+++ packageObjects/comment_test.go
@@ -5,3 +5,31 @@ Matrikelnummern:
 - 3886565
 */
 package packageObjects
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestCommentsSize(t *testing.T) {
+	assert.Equal(t, 4, len(*GetAllCommentsByUser("test")))
+	assert.Equal(t, 0, len(*GetAllCommentsByUser("test1")))
+}
+
+func TestFilterCommentByHash(t *testing.T) {
+	allComments := GetAllCommentsByUser("test")
+
+	comments := FilterAllCommentsByHash(allComments, "fb891262c98f9725b54a613c6f1cbfb8c701bca2d386a55cd0d1b4966180549d")
+
+	assert.Equal(t, true, comments != nil)
+
+	assert.Equal(t, "Gutes Bild", (*comments)[0].Comment)
+	assert.Equal(t, "2021.01.10 16:34:37", (*comments)[0].Date)
+	assert.Equal(t, "fb891262c98f9725b54a613c6f1cbfb8c701bca2d386a55cd0d1b4966180549d", (*comments)[0].Hash)
+
+	assert.Equal(t, "Super Bild", (*comments)[1].Comment)
+	assert.Equal(t, "2021.01.10 16:34:49", (*comments)[1].Date)
+	assert.Equal(t, "fb891262c98f9725b54a613c6f1cbfb8c701bca2d386a55cd0d1b4966180549d", (*comments)[1].Hash)
+
+	assert.Equal(t, true, FilterAllCommentsByHash(allComments, "abc") == nil)
+}
