38327e662e948ea75e20915138d1b2e6f4c78e4d
Yannis <admin@coding-hard.com>
2021-01-10 18:21:50 +0100

    add tests for photo

--- packageObjects/photo_test.go
+++ packageObjects/photo_test.go
@@ -5,3 +5,32 @@ Matrikelnummern:
 - 3886565
 */
 package packageObjects
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestPhotosSize(t *testing.T) {
+	assert.Equal(t, 3, len(*GetAllPhotosByUser("test")))
+}
+
+func TestPhotosPageAmount(t *testing.T) {
+	assert.Equal(t, 1, GetPhotoPageAmount("test"))
+}
+
+func TestPhotosForPage(t *testing.T) {
+	assert.Equal(t, 3, len(*GetPhotosForPage("test", 1)))
+}
+
+func TestGetPhotoByUserAndHash(t *testing.T) {
+	photos := GetAllPhotosByUser("test")
+	photo1 := GetPhotoByUserAndHash(photos, "fb891262c98f9725b54a613c6f1cbfb8c701bca2d386a55cd0d1b4966180549d")
+
+	assert.Equal(t, false, photo1 == nil)
+	assert.Equal(t, "DSC_0053.JPG", photo1.Name)
+
+	photo2 := GetPhotoByUserAndHash(photos, "abc")
+
+	assert.Equal(t, true, photo2 == nil)
+}
